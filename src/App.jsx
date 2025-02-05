1f6803dd41f621206c73ef8fddcb11d024c18c51
UlianaAsadulina <u.asadulina@icloud.com>
2025-02-05 16:44:16 -0500

    Start point

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>ALAB 320H.3.1 - Rendering Arrays from State</h1>
+    </>
+  )
+}
+
+export default App
